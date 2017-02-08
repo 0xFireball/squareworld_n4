f4b896c1c2cbebdab59ddd854db611876c527ba2
0xFireball <0xFireball@outlook.com>
2017-02-08 12:57:15 -0800

    hit player

--- Sources/sprites/Bullet.hx
+++ Sources/sprites/Bullet.hx
@@ -3,6 +3,7 @@ package sprites;
 import kha.Color;
 import n4.effects.particles.NSquareParticleEmitter;
 import n4.entities.NSprite;
+import n4.NGame;
 import n4.util.NColorUtil;
 
 class Bullet extends NSprite {
@@ -13,15 +14,30 @@ class Bullet extends NSprite {
 	}
 
 	public override function update(dt:Float) {
-		// for (i in 0...9) {
-		// 	Registry.PS.emitter
-		// 		.emit(x, y, 9, NSquareParticleEmitter.velocitySpread(75, 
-		// 				Registry.PS.player.velocity.x / 2,
-		// 				Registry.PS.player.velocity.y / 2),
-		// 			NColorUtil.randCol(1, 0.1, 0),
-		// 			0.8
-		// 		);
-		// }
+		var hitRangeF = 1.5;
+		if (Math.abs(Registry.PS.player.x - this.x) < Registry.PS.player.width / hitRangeF && Math.abs(Registry.PS.player.y - this.y) < Registry.PS.player.height / hitRangeF) {
+			// hit player
+			Registry.PS.player.x = (Math.random() * 0.8 + 0.1) * NGame.width;
+			Registry.PS.player.y = (Math.random() * 0.8 + 0.1) * NGame.height;
+			for (i in 0...15) {
+				Registry.PS.emitter
+					.emit(x, y, 9, NSquareParticleEmitter.velocitySpread(100, 
+							Registry.PS.player.velocity.x / 2,
+							Registry.PS.player.velocity.y / 2),
+						NColorUtil.randCol(0.3, 0.3, 0.3),
+						0.8
+					);
+			}
+			for (i in 0...9) {
+				Registry.PS.emitter
+					.emit(x, y, 9, NSquareParticleEmitter.velocitySpread(75, 
+							Registry.PS.player.velocity.x / 2,
+							Registry.PS.player.velocity.y / 2),
+						NColorUtil.randCol(1, 0.1, 0),
+						0.8
+					);
+			}
+		}
 		Registry.PS.emitter
 			.emit(x, y, 3, NSquareParticleEmitter.velocitySpread(50),
 				NColorUtil.randCol(0.7, 0, 0),
