02ad1ce4737c48dcf2d20bc996fbae591fba648b
0xFireball <0xFireball@outlook.com>
2017-02-08 11:28:40 -0800

    turret particles

--- Sources/PlayState.hx
+++ Sources/PlayState.hx
@@ -1,9 +1,12 @@
 package;
 
 import kha.Color;
+import kha.math.*;
 import n4.NState;
+import n4.effects.particles.NSquareParticleEmitter;
 import n4.group.NTypedGroup;
 import n4.NGame;
+import n4.util.NColorUtil;
 import sprites.*;
 
 class PlayState extends NState {
@@ -11,6 +14,9 @@ class PlayState extends NState {
 	private var _player:Player;
 	private var _bullets:NTypedGroup<Bullet>;
 	private var _turret:Turret;
+	private var _emitter:NSquareParticleEmitter;
+
+	private var _frameCount = 0;
 
 	override public function create() {
 		bgColor = Color.fromValue(0xFFEEEEEE);
@@ -24,6 +30,22 @@ class PlayState extends NState {
 		_bullets = new NTypedGroup<Bullet>();
 		add(_bullets);
 
+		_emitter = new NSquareParticleEmitter();
+		add(_emitter);
+
 		super.create();
+	}
+
+	override public function update(dt:Float) {
+		super.update(dt);
+
+		++_frameCount;
+		if (_frameCount % 12 == 0) {
+			for (i in 0...20) {
+				_emitter.emit(_turret.x + _turret.width / 2, _turret.y + _turret.height / 2, 6,
+					_emitter.velocitySpread(45),
+					NColorUtil.randCol(0.5, 0.5, 0.5), 0.6);
+			}
+		}
 	}
 }
