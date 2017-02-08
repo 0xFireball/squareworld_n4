483d81b088a1af466bb5b4e81c0305eaefed85f6
0xFireball <0xFireball@outlook.com>
2017-02-07 19:41:46 -0800

    movement

--- Sources/sprites/Player.hx
+++ Sources/sprites/Player.hx
@@ -1,12 +1,17 @@
 package sprites;
 
 import kha.Color;
+import kha.input.Keyboard;
+import kha.Key;
 import n4.entities.NSprite;
 
 class Player extends NSprite {
+
 	public function new(?X:Float = 0, ?Y:Float = 0) {
 		super(X, Y);
 
-		makeGraphic(20, 20, Color.Blue);
+		makeGraphic(20, 20, Color.Red);
+		velocity.x = 300;
+		acceleration.x = -140;
 	}
 }
