557e82115edac3bfddb3190d57abfc705098257a
0xFireball <0xFireball@outlook.com>
2017-02-09 18:20:11 -0800

    implement lookahead targeting

--- Sources/PlayState.hx
+++ Sources/PlayState.hx
@@ -23,7 +23,7 @@ class PlayState extends NState {
 
 		bgColor = Color.fromValue(0xFFEEEEEE);
 
-		player = new Player();
+		player = new Player(Math.random() * NGame.width, Math.random() * NGame.height);
 		add(player);
 
 		_turret = new Turret(NGame.width / 2 - 10, NGame.height / 2 - 10);
@@ -60,10 +60,58 @@ class PlayState extends NState {
 
 	function turretFire() {
 		var bulletSp = 350;
-		var dx = (_turret.x + _turret.width / 2) - (player.x + player.width / 2);
-		var dy = (_turret.y + _turret.height / 2) - (player.y + player.height / 2);
-		var m = -Math.sqrt(dx*dx + dy*dy);
-		shoot(_turret.x + _turret.width / 2, _turret.y + _turret.height / 2, dx*bulletSp/m, dy*bulletSp/m);
+		// var fancy = player.velocity.length > 0;
+		var fancy = false;
+		if (!fancy) {
+			var dx = (_turret.x + _turret.width / 2) - (player.x + player.width / 2);
+			var dy = (_turret.y + _turret.height / 2) - (player.y + player.height / 2);
+			var m = -Math.sqrt(dx*dx + dy*dy);
+			shoot(_turret.x + _turret.width / 2, _turret.y + _turret.height / 2, dx*bulletSp/m, dy*bulletSp/m);
+		} else {
+			var px:Float = player.x;
+			var py:Float = player.y;
+			var vx:Float = player.velocity.x;
+			var vy:Float = player.velocity.y;
+			var m:Float = Math.sqrt(vx * vx + vy * vy);
+			var sin = m == 0 ? 0 : vy / m;
+			var cos = m == 0 ? 0 : vx / m;
+			var rot = function (x:Float, y:Float) {
+				return [x * cos + y * sin, y * cos - x * sin];
+			}
+			var vr1 = rot(vx, vy);
+			vx = vr1[0];
+			vy = vr1[1];
+			var vr2 = rot(px, py);
+			px = vr1[0];
+			py = vr1[1];
+
+			py = -py;
+			var adf2 = 2 * vx * px / py;
+			var df = 1 + (px * px) / (py * py);
+
+			// calculate the result
+			var disc = adf2 * adf2 + 4 * df * (bulletSp * bulletSp - vx * vx);
+			if (disc < 0) return;
+			disc = Math.sqrt(disc);
+			df *= 2;
+			sin = -sin; // invert rot
+
+			var c1 = (adf2 + disc) / df;
+			var b1 = vx - c1 * px / py;
+			var t1 = py / c1;
+			var res1 = rot(b1, c1);
+			var c2 = (adf2 - disc) / df;
+			var b2 = vx - c2 * px / py;
+			var t2 = py / c2;
+			var res2 = rot(b2, c2);
+
+			var ret = [];
+			if (t1 < 0) ret = res2;
+  			else if (t2 < 0) ret = res1;
+  			else ret = t1 < t2 ? res1 : res2;
+			
+			shoot(_turret.x + _turret.width / 2, _turret.y + _turret.height / 2, ret[0], ret[1]);
+		}
 	}
 
  	function shoot(x:Float, y:Float, dx:Float, dy:Float):Void {
