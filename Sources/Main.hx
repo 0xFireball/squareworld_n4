6e1200730f9790079cd0f45ed094d3c80717dba2
0xFireball <0xFireball@outlook.com>
2017-02-12 12:36:31 -0800

    fix issue with preloader

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -5,11 +5,12 @@ import n4.NGame;
 class Main {
 	public static function main() {
 		NGame.init("shapes2", 
-		#if sys_html5
-			0, 0,
-		#else
-			1024, 768,
-		#end
+		// #if sys_html5
+		// 	0, 0,
+		// #else
+		// 	1024, 768,
+		// #end
+		1024, 768,
 			PlayState, 60);
 	}
 }
