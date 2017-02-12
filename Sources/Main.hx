f13b68c87cfd4c8275c292d7b21d21a59a3019cd
0xFireball <0xFireball@outlook.com>
2017-02-12 12:37:50 -0800

    use fullscreen html5

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -5,12 +5,11 @@ import n4.NGame;
 class Main {
 	public static function main() {
 		NGame.init("shapes2", 
-		// #if sys_html5
-		// 	0, 0,
-		// #else
-		// 	1024, 768,
-		// #end
-		1024, 768,
+		#if sys_html5
+			0, 0,
+		#else
+			1024, 768,
+		#end
 			PlayState, 60);
 	}
 }
