7429572f63a82b26c231b431ef9a05ee3d286ebd
0xFireball <0xFireball@outlook.com>
2017-06-20 09:41:14 -0700

    update size setting in n4

--- Sources/sprites/Bullet.hx
+++ Sources/sprites/Bullet.hx
@@ -20,6 +20,15 @@ class Bullet extends NSprite {
 				NColorUtil.randCol(0.7, 0, 0),
 				0.3
 			);
+
+		// if (x > Registry.PS.player.x
+		// 	&& x < Registry.PS.player.x + Registry.PS.player.width
+		// 	&& y > Registry.PS.player.y
+		// 	&& y < Registry.PS.player.y + Registry.PS.player.height)
+		// {
+			
+		// }
+
 		super.update(dt);
 	}
 }
